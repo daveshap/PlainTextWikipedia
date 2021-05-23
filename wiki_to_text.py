7e6ec7ddaf2e9c7f6daaf0807fc611e1b86fedcf
David Shapiro <daveshap37@gmail.com>
2021-05-23 10:20:03 -0400

    small update

--- wiki_to_text.py
+++ wiki_to_text.py
@@ -1,7 +1,8 @@
 from dewiki_functions import *
 
-#filename = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
-filename = 'G:/enwiki-20210401/enwiki-20210401.xml'  # update this
+#wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
+wiki_xml_file = 'G:/enwiki-20210401/enwiki-20210401.xml'  # update this
+json_save_dir = 'G:/wiki_plaintext/'
 
 if __name__ == '__main__':
-    process_file_text(filename, 'G:/wiki_plaintext/')
+    process_file_text(wiki_xml_file, json_save_dir)
